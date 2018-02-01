c4bddbf6614df25274d538c2b236a6f3a3d418a2
nitrofortin <julienstpierrefortin@gmail.com>
2018-01-31 20:56:21 -0500

    first test

--- tests/test_frame.py
+++ tests/test_frame.py
@@ -1,4 +1,18 @@
+import pandas as pd
+import numpy as np
+import pytest
+
 from dataflow.frame import SmartDataFrame
 
-def test_label_encoded():
-	
+
+@pytest.fixture
+def data():
+	size = 100
+	data = pd.DataFrame()
+	data['label'] = ['a','b','c','d','e']*(size/5)
+	data['numeric'] = np.arange(size)
+	return SmartDataFrame(data=data)
+
+
+def test_label_encode(data):
+	data.label_encode(features='label')
